bcec053382c0bd0e9e630476006301b261d89187
Yurii Rysyn <yurii.rysyn@yanpix.com>
2022-02-23 22:12:15 +0200

    Updates

---
+++ precache-manifest.88cb83116fa74b21a97ef90c20fd9fde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdc2b63980390f7e66a8d44736d64f83",
+    "url": "/CryptoPriceApp/index.html"
+  },
+  {
+    "revision": "a23fb183cf4a7c020dc4",
+    "url": "/CryptoPriceApp/static/css/main.1f4aa070.chunk.css"
+  },
+  {
+    "revision": "c22e6e9f3cb40c60fcb1",
+    "url": "/CryptoPriceApp/static/js/2.58422421.chunk.js"
+  },
+  {
+    "revision": "e3d485443dd897dbed9c3738a7076a3f",
+    "url": "/CryptoPriceApp/static/js/2.58422421.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a23fb183cf4a7c020dc4",
+    "url": "/CryptoPriceApp/static/js/main.ab44bb83.chunk.js"
+  },
+  {
+    "revision": "0e377666a3db54aa0767",
+    "url": "/CryptoPriceApp/static/js/runtime-main.a99ab7e9.js"
+  }
+]);
